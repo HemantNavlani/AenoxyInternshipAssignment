3ffb0f5440a5ced172fda67cf45def9539cf941b
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-04-07 01:49:18 +0530

    Sign Up page done

--- aenoxy_assignment/src/main.jsx
+++ aenoxy_assignment/src/main.jsx
@@ -2,9 +2,71 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import SignupPage from './pages/SignupPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {
+        path:'/',
+        element:<SignupPage/>
+      },
+      // {
+      //   path:'/login',
+      //   element:(
+      //     <AuthLayout authentication={false}>
+      //       <Login/>
+      //     </AuthLayout>
+      //   )
+      // },
+      // {
+      //   path:'/signup',
+      //   element:(
+      //     <AuthLayout authentication={false}>
+      //       <Signup />
+      //     </AuthLayout>
+      //   )
+      // },
+      // {
+      //   path:'/all-posts',
+      //   element:(
+      //     <AuthLayout authentication>
+      //       {" "}
+      //       <AllPosts/>
+      //     </AuthLayout>
+      //   )
+      // },
+      // {
+      //   path:'/add-post',
+      //   element:(
+      //     <AuthLayout authentication>
+      //       {" "}
+      //       <AddPost/>
+      //     </AuthLayout>
+      //   )
+      // },
+      // {
+      //   path:'/edit-post/:slug',
+      //   element:(
+      //     <AuthLayout authentication>
+      //       {' '}
+      //       <EditPost/>
+      //     </AuthLayout>
+      //   )
+      // },
+      // {
+      //   path:'/post/:slug',
+      //   element:<Post/>
+      // }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router}/>
   </React.StrictMode>,
 )
